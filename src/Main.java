fddef45be905d097761a0506e9e6b1b7023b3315
MIhail <skuter63@mail.ru>
2021-09-04 21:48:46 +0400

    data

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println("Введите данные для расчета");
+        String data = scanner.nextLine();
+
+        System.out.println("Вот такие данные ввели " + data);
     }
 }
