6516e3db035fd33418897515c31b88beeac26e6b
MIhail <skuter63@mail.ru>
2021-09-05 00:17:39 +0400

    add perem

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите данные для расчета");
         String data = scanner.nextLine();
+        //int intData = Integer.parseInt(data);
+        char[] strMyArray = data.toCharArray();
 
-        System.out.println("Вот такие данные ввели " + data);
+        int oneperem = Integer.parseInt(String.valueOf(strMyArray[0]));
+        char twoperem = strMyArray[1];
+//        int twoperem = Integer.parseInt(String.valueOf(strMyArray[1]));
+        int threeper = Integer.parseInt(String.valueOf(strMyArray[strMyArray.length -1]));
+
+
+        System.out.println(oneperem);
+        System.out.println(twoperem);
+        System.out.println(threeper);
+
+
+
+//        for (int i =0; i < strMyArray.length; i++){
+//
+//            System.out.print(strMyArray[i]);
+//        }
+
+//        System.out.println("Вот такие данные ввели " + data);
+        // Будем использовать массив
+        scanner.close();
     }
 }
